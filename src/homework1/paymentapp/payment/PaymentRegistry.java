dbd3618913768445fa8a2700674a12fbf60dbfbd
elifnurdonmez <elif9881@gmail.com>
2026-04-19 11:52:07 +0300

    Reflection feature has added

--- src/homework1/paymentapp/payment/PaymentRegistry.java
+++ src/homework1/paymentapp/payment/PaymentRegistry.java
@@ -2,6 +2,7 @@ package homework1.paymentapp.payment;
 
 import homework1.paymentapp.payment.enums.PaymentType;
 
+import java.lang.reflect.Constructor;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,10 +10,33 @@ public class PaymentRegistry {
     private final Map<PaymentType, PaymentMethod> payments = new HashMap<>();
 
     public PaymentRegistry() {
-        payments.put(PaymentType.APPLE_PAY, new ApplePay());
-        payments.put(PaymentType.CREDIT_CARD, new CreditCard());
-        payments.put(PaymentType.DEBIT_CARD, new DebitCard());
-        payments.put(PaymentType.PAYPAL, new PayPal());
+        autoRegister();
+    }
+
+    private void autoRegister() {
+        try {
+            for (PaymentType type : PaymentType.values()) {
+                String className = "homework1.paymentapp.payment." + formatClassName(type.name());
+
+                Class<?> clazz = Class.forName(className);
+                Constructor<?> constructor = clazz.getConstructor();
+                PaymentMethod instance = (PaymentMethod) constructor.newInstance();
+
+                payments.put(type, instance);
+            }
+        } catch (Exception e) {
+            System.err.println("Reflection hatası: Bazı ödeme yöntemleri yüklenemedi.");
+        }
+    }
+
+    private String formatClassName(String enumName) {
+        StringBuilder sb = new StringBuilder();
+        String[] parts = enumName.split("_");
+        for (String part : parts) {
+            sb.append(part.substring(0, 1).toUpperCase())
+                    .append(part.substring(1).toLowerCase());
+        }
+        return sb.toString();
     }
 
     public PaymentMethod get(PaymentType type) {
--- src/homework1/paymentapp/payment/Paypal.java
+++ src/homework1/paymentapp/payment/Paypal.java
@@ -2,7 +2,7 @@ package homework1.paymentapp.payment;
 
 import homework1.paymentapp.payment.enums.Currency;
 
-public class PayPal implements PaymentMethod {
+public class Paypal implements PaymentMethod {
     @Override
     public String pay(double tutar, Currency currency) {
         return String.format("PayPal ile %.2f %s değerindeki ödemeniz başarılıdır.", tutar, currency);
